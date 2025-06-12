07d80e7d1a58b4e9b6a383dd6198752cde752545
mohamedemad2251 <mohamedemad2251@gmail.com>
2025-06-12 16:58:00 +0300

    Added HTML attributes for template_content + HTML widget in the form view

--- models/letter_template.py
+++ models/letter_template.py
@@ -6,11 +6,12 @@ class LetterTemplate(models.Model):
 
     template_name = fields.Char(string="Template Name")
     template_date = fields.Date(string="Template Date")
-    template_content = fields.Html(string="Template Content - HTML")
+    template_content = fields.Html(string="Template Content - HTML", default="", sanitize=False, sanitize_tags=False, sanitize_attributes=False)
     template_placeholders = fields.Selection(string="Template Placeholders", selection=[('*selection_one*','Selection One'),('*selection_two*','Selection Two')])
 
     @api.onchange('template_placeholders')
     def _embed_template_placeholder(self):
         for record in self:
-            record.template_content+=record.template_placeholders
-            record.template_placeholders=None
+            if(record.template_placeholders):
+                record.template_content += record.template_placeholders
+                record.template_placeholders=None
