d746b693da0dcfa0d3b5d0ca3f61a36068f34087
mohamedemad2251 <mohamedemad2251@gmail.com>
2025-07-10 02:44:55 +0300

    Fixed Letter Writer - HR. As well as changing the entire placeholders/logic and dynamic harnessing infrastructure

--- models/letter_letter.py
+++ models/letter_letter.py
@@ -1,5 +1,6 @@
 from dateutil.utils import today
-from odoo import api,fields,models
+from odoo import api, fields, models
+
 
 class LetterLetter(models.Model):
     _name = 'letter.letter'
@@ -7,39 +8,46 @@ class LetterLetter(models.Model):
 
     _inherit = 'letter.base'
 
-    letter_name=fields.Char(string="Letter Name", readonly=True, copy=False, compute='compute_letter_code',store=True)
-    letter_date=fields.Date(string="Letter Date", default=today())
-    template_id=fields.Many2one('letter.template',string="Template",required=True)
-    linked_content=fields.Html(readonly=True, related='template_id.template_content')
+    letter_name = fields.Char(string="Letter Name", readonly=True, copy=False, compute='compute_letter_code',
+                              store=True)
+    letter_date = fields.Date(string="Letter Date", default=today())
+    template_id = fields.Many2one('letter.template', string="Template", required=True)
+    linked_content = fields.Html(readonly=True, related='template_id.template_content')
 
-    _sql_constraints = [('unique_letter_code','UNIQUE(letter_name)','Letter Name should be unique! (Hint: It already exists)')]
+    template_module = fields.Selection(related='template_id.template_module', store=False)  #Dummy related field to be able to use it in XPath correctly with invisible attr logic
+
+    _sql_constraints = [
+        ('unique_letter_code', 'UNIQUE(letter_name)', 'Letter Name should be unique! (Hint: It already exists)')]
+
 
     def print_letter(self):
         return self.env.ref('letter_writer.letter_html_report').report_action(self)
 
     def save_letter_docx(self):
         return {
-            'type' : 'ir.actions.act_url',      #Type: URL (To send the request to the HTTP Controller we created in the controllers
-            'url' : f'/letter/{self.id}/download_docx',     #URL endpoint. Will be heard by the controller
-            'target' : 'self'       #self = Same Window
+            'type': 'ir.actions.act_url',
+            #Type: URL (To send the request to the HTTP Controller we created in the controllers
+            'url': f'/letter/{self.id}/download_docx',  #URL endpoint. Will be heard by the controller
+            'target': 'self'  #self = Same Window
         }
 
     #This method is the actual backend implementation for letter_name. _change_letter_code changes the letter name in the UI
     #at real time, but this changes it in the record itself
-    @api.depends('template_id','letter_date')
+    @api.depends('template_id', 'letter_date')
     def compute_letter_code(self):
         for record in self:
-            if record.template_id and record.letter_date:   #TO-DO: Add validation for record.template_id.next_number
-                record.letter_name = record.template_id.template_code + '/' + str(record.letter_date.year) + '/' + str(record.template_id.next_number)
+            if record.template_id and record.letter_date:  #TO-DO: Add validation for record.template_id.next_number
+                record.letter_name = record.template_id.template_code + '/' + str(record.letter_date.year) + '/' + str(
+                    record.template_id.next_number)
                 record.template_id.next_number += 1
             else:
                 record.letter_name = None
 
     #This method helps with the visualization for the user in real-time while changing templates. It doesn't affect the database or
     #backend.
-    @api.onchange('template_id','letter_date')
+    @api.onchange('template_id', 'letter_date')
     def _change_letter_code(self):
         for record in self:
             if record.template_id and record.letter_date:
                 record.letter_name = (record.template_id.template_code + '/' + str(record.letter_date.year)
-                                  + '/' + str(record.template_id.next_number))
+                                      + '/' + str(record.template_id.next_number))
--- models/letter_template.py
+++ models/letter_template.py
@@ -3,49 +3,58 @@ from dateutil.utils import today
 
 from odoo import api, fields, models
 from odoo.exceptions import UserError
-from odoo.tools.view_validation import validate
 
 
 class LetterTemplate(models.Model):
     _name = 'letter.template'
     _description = "Letter Templates Model"
-    _rec_name = 'template_name' #Your template name isn't called "name" so Odoo's ORM by default can't grab records/show their names. We need to represent records with a variable assigned.
+    _rec_name = 'template_name'  #Your template name isn't called "name" so Odoo's ORM by default can't grab records/show their names. We need to represent records with a variable assigned.
 
     template_name = fields.Char(string="Template Name", required=True)
     template_date = fields.Date(string="Template Date", default=today())
-    template_content = fields.Html(string="Template Content", sanitize=False, sanitize_tags=False, sanitize_attributes=False, required=True)
-    template_placeholders = fields.Selection(
-        string="Template Placeholders",
-        selection=lambda self: self._get_dynamic_placeholders_selection(),
-        help="Select a placeholder that will be changed dynamically in your letter. Placeholders have special syntax to be detected"
-              "later. Please don't change the syntax as it won't be replaced later on."
-     )
+    template_content = fields.Html(string="Template Content", sanitize=False, sanitize_tags=False,
+                                   sanitize_attributes=False, required=True)
+    template_module = fields.Selection(string="Template Module", required=True,
+                                       selection=lambda self: self._get_dynamic_modules_selection(), readonly=False)
+
+    template_placeholders_id = fields.Many2one('letter.placeholders',string="Template Placeholders")
+
     template_code = fields.Char(string="Template Code", required=True)
     next_number = fields.Integer(string="Next Allowed Number")
 
+
     #Adds a layer of validation using the query UNIQUE. This checks if the table already has that template_code field value or not. If it does, it throws a validation error.
-    _sql_constraints = [('unique_template_code','UNIQUE(template_code)',"Template\'s code must be unique! (Hint: This code already exists)")]
+    _sql_constraints = [('unique_template_code', 'UNIQUE(template_code)',
+                         "Template\'s code must be unique! (Hint: This code already exists)")]
 
+    @api.model
+    def _module_is_installed(self, module_name):    #Extra method to check ir.module.module records if a module (given by module_name) is installed or not.
+        return self.env['ir.module.module'].sudo().search_count([
+            ('name', '=', module_name),
+            ('state', '=', 'installed')]
+        ) > 0
 
-    def _get_dynamic_placeholders_selection(self):
-        # Create a fake letter record to access get_letter_placeholders
-        letter_model = self.env['letter.letter']
-        letter_sample = letter_model.new({})  # empty in-memory record
+    def _get_dynamic_modules_selection(self):       #Dynamic dropdown depending on the modules installed (extensions of Letter Writer)
+        selection = [
+            ('base', 'No Module'),
+        ]
+        if self._module_is_installed('letter_hr'):
+            selection.append(('hr','HR'))
+        return selection
 
-        placeholder_dict = letter_sample.get_letter_placeholders()
-        return [(key, key.replace("*", "").replace("_", " ").title()) for key in placeholder_dict.keys()]
-
-    @api.onchange('template_placeholders')
+    @api.onchange('template_placeholders_id')
     def _embed_template_placeholder(self):
         string_added = ""
         for record in self:
-            if record.template_placeholders:   #Protects UI from having undefined behavior since a selection is by default None
-                if not record.template_content:        #If no content has been added yet (new template/deleted content)
-                    record.template_content = markupsafe.Markup('') #Transform the template_content from 'bool' to 'Markup'
+            if record.template_placeholders_id:  #Protects UI from having undefined behavior since a selection is by default None
+                if not record.template_content:  #If no content has been added yet (new template/deleted content)
+                    record.template_content = markupsafe.Markup(
+                        '')  #Transform the template_content from 'bool' to 'Markup'
                 record.template_content = markupsafe.Markup(record.template_content)
-                string_added = markupsafe.Markup('<span>') + markupsafe.Markup(record.template_placeholders) + markupsafe.Markup('</span>')
+                string_added = markupsafe.Markup('<span>') + markupsafe.Markup(
+                    record.template_placeholders_id.code) + markupsafe.Markup('</span>')
                 record.template_content = record.template_content + string_added  #Concatenate the placeholder
-                record.template_placeholders=None           #Reset the dropdown to None (nothing shows up, like the selection took effect & vanished)
+                record.template_placeholders_id = None  #Reset the dropdown to None (nothing shows up, like the selection took effect & vanished)
 
     @api.onchange('template_code')
     def _capitalize_template_code(self):
